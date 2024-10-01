8626b83c49ea607dbd8c24b704f1034145bc3b0c
Ertuğrul Şengül <ertugrul.sengul00@gmail.com>
2024-10-01 18:28:04 +0300

    stack 20 added

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
-import LinkedList.medium.LL_2;
-import LinkedList.medium.LL_328;
-import LinkedList.medium.ListNode;
+import Stack.easy.stack_20;
 
 
 public class Main {
     public static void main(String[] args) {
         // Write testcases here
-        ListNode a = new ListNode(1);
-        ListNode b = new ListNode(2);
-        ListNode c = new ListNode(3);
-        ListNode d = new ListNode(4);
-        ListNode e = new ListNode(5);
-        ListNode f = new ListNode(4);
-        ListNode g = new ListNode(7);
+        stack_20 solution = new stack_20();
 
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-        e.next = null;
-        f.next = g;
-        g.next = null;
-
-        LL_328 sol = new LL_328();
-        sol.oddEvenList(a);
-        System.out.println("Hello world");
+        String test = "()[]{}";
+        System.out.println(solution.isValid(test));
     }
 }
---
+++ src/Stack/easy/stack_20.java
@@ -0,0 +1,27 @@
+package Stack.easy;
+
+import java.util.Stack;
+
+public class stack_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    char closed = stack.pop();
+                    if((c == ']' && closed != '[') || (c == '}' && closed != '{') || (c == ')' && closed != '(')) return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
